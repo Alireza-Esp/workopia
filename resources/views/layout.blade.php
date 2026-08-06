2449c2becd8c68a994fb4520bf895f575f73ba12
Alireza Esp <alireza.esp07@gmail.com>
2026-08-06 15:41:33 +0330

    add theme files and add header codes from theme to component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://lib.arvancloud.ir/font-awesome/6.3.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
 
