de392890b7a7138ea96d14adb97253a958ed3d77
Alireza Esp <alireza.esp07@gmail.com>
2026-08-07 15:24:51 +0330

    add hero component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <!-- <script src="https://lib.arvancloud.ir/font-awesome/6.3.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
 
 <body class="bg-gray-100">
     <x-header />
+    @if(request()->is('/'))
+    <x-hero></x-hero>
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
