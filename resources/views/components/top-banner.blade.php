6699c84a632ff55127bcc0078e95bfca3e378e1c
Alireza Esp <alireza.esp07@gmail.com>
2026-08-07 15:48:32 +0330

    add top and bottom banner

---
+++ resources/views/components/top-banner.blade.php
@@ -0,0 +1,18 @@
+@props([
+    'heading' => 'Unlock Your Career Potential',
+    'tagline' => 'Discover the perfect job opportunity for you.'
+])
+
+<section class="bg-blue-900 text-white py-6 text-center">
+    <div class="container mx-auto">
+        <h2 class="text-3xl font-semibold">
+            {{ $heading }}
+        </h2>
+        <p class="text-lg mt-2">
+            {{ $tagline }}
+        </p>
+    </div>
+</section>
+<div>
+    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
+</div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
     <h1>Index</h1>
+
+    <x-bottom-banner></x-bottom-banner>
 </x-layout>
