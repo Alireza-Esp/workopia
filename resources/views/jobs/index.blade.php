48b772b0d98c62fbf8ecd53f5a4a15e6f6030a32
Alireza Esp <alireza.esp07@gmail.com>
2026-08-08 17:59:45 +0330

    create job listings model

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,3 +1,10 @@
 <x-layout>
-    
+    <h1>Aavailable Jobs</h1>
+    <ul>
+        @forelse ($jobs as $job)
+            <li>{{ $job->title }} - {{ $job->description }}</li>
+        @empty
+            <li>No Jobs Available</li>
+        @endforelse
+    </ul>
 </x-layout>
