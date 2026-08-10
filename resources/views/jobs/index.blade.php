501d5463211906d2842a1ab46f7581dd0bc824dd
Alireza Esp <alireza.esp07@gmail.com>
2026-08-10 11:24:49 +0330

    add some validations for create jobs

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,7 +2,7 @@
     <h1>Aavailable Jobs</h1>
     <ul>
         @forelse ($jobs as $job)
-            <li>{{ $job->title }} - {{ $job->description }}</li>
+            <li><a href="{{ url("/jobs/$job->id") }}">{{ $job->title }}</a> - {{ $job->description }}</li>
         @empty
             <li>No Jobs Available</li>
         @endforelse
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
-    <h1 class="text-2xl"><a href="{{ route('jobs.show', $job->id) }}</a></h1>
+    <h1 class="text-2xl"><a href="{{ route('jobs.show', $job->id) }}">{{ $job->title }}</a></h1>
     <p>{{ $job->description }}</p>
 </x-layout>
