91a79d72b4e31215c4454f88daf2f62e67c3a2c7
Alireza Esp <alireza.esp07@gmail.com>
2026-08-10 17:00:51 +0330

    design home index view

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <h2 class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">
-        Recent Jobs
+        All Jobs
     </h2>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
     @forelse ($jobs as $job)
@@ -9,4 +9,6 @@
         <p>No Jobs Available</p>
     @endforelse
     </div>
+
+    <x-bottom-banner></x-bottom-banner>
 </x-layout>
