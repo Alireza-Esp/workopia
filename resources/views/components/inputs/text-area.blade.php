c1dcce8950eab7a2e27764367bc44935af0a69f1
Alireza Esp <alireza.esp07@gmail.com>
2026-08-11 19:13:55 +0330

    make select input components and use in create job page

--- resources/views/components/inputs/text-area.blade.php
+++ resources/views/components/inputs/text-area.blade.php
@@ -12,7 +12,7 @@
     <label class="block text-gray-700" for="{{ $id }}">{{ $label }}</label>
     <textarea  @if($large) cols="30" rows="7" @endif  id="{{ $id }}" name="{{ $name }}"
         class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror"
-        placeholder="{{ $placeholder }}"></textarea>
+        placeholder="{{ $placeholder }}">{{ old($name) }}</textarea>
     @error($name)
         <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
     @enderror
--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -21,31 +21,9 @@
 
             <x-inputs.text id="tags" name="tags" label="Tags (comma-separated)" placeholder="development,coding,java,python"></x-inputs.text>
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="job_type">Job Type</label>
-                <select id="job_type" name="job_type" class="w-full px-4 py-2 border rounded focus:outline-none @error('job_type') border-red-500 @enderror">
-                    <option value="Full-Time" {{ old('job_type') == 'Full-Time' ? 'selected' : '' }}>
-                        Full-Time
-                    </option>
-                    <option value="Part-Time" {{ old('job_type') == 'Part-Time' ? 'selected' : '' }}>Part-Time</option>
-                    <option value="Contract" {{ old('job_type') == 'Contract' ? 'selected' : '' }}>Contract</option>
-                    <option value="Temporary {{ old('job_type') == 'Temporary' ? 'selected' : '' }}">Temporary</option>
-                    <option value="Internship" {{ old('job_type') == 'Internship' ? 'selected' : '' }}>Internship</option>
-                    <option value="Volunteer" {{ old('job_type') == 'Volunteer' ? 'selected' : '' }}>Volunteer</option>
-                    <option value="On-Call" {{ old('job_type') == 'On-Call' ? 'selected' : '' }}>On-Call</option>
-                </select>
-                @error('job_type')
-                    <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
-                @enderror
-            </div>
+            <x-inputs.select id="job_type" name="job_type" :options="['Full-Time', 'Part-Time', 'Contract', 'Temporary', 'Internship', 'Volunteer', 'On-Call']" label="Job Type"></x-inputs.select>
 
-            <div class="mb-4">
-                <label class="block text-gray-700" for="remote">Remote</label>
-                <select id="remote" name="remote" class="w-full px-4 py-2 border rounded focus:outline-none">
-                    <option value="false">No</option>
-                    <option value="true">Yes</option>
-                </select>
-            </div>
+            <x-inputs.select id="remote" name="remote" :options="['No', 'Yes']" label="Remote"></x-inputs.select>
 
             <x-inputs.text id="address" name="address" label="Address" placeholder="123 Main St"></x-inputs.text>
 
