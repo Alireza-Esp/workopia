957f6c2f35092821866cd26df0c71f99210f4e02
Alireza Esp <alireza.esp07@gmail.com>
2026-08-12 13:36:43 +0330

    design update page and add funcionality. fix bugs for input components

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Job;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Storage;
 
 class JobController extends Controller
 {
@@ -77,17 +79,48 @@ class JobController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Job $job): View
     {
-        //
+        return view('jobs.edit')->with('job', $job);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Job $job)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'salary' => 'required|integer',
+            'tags' => 'nullable|string',
+            'job_type' => 'required|string',
+            'remote' => 'required|boolean',
+            'requirements' => 'nullable|string',
+            'benefits' => 'nullable|string',
+            'address' => 'nullable|string',
+            'city' => 'required|string',
+            'state' => 'required|string',
+            'zipcode' => 'nullable|string',
+            'contact_email' => 'required|string',
+            'contact_phone' => 'nullable|string',
+            'company_name' => 'required|string',
+            'company_description' => 'nullable|string',
+            'company_logo' => 'nullable|image|mimes:jpeg,jpg,png|max:2048',
+            'company_website' => 'nullable|url',
+        ]);
+
+        if (request()->hasFile('company_logo')) {
+            Storage::delete('public/logos/' . basename($job->company_logo));
+
+            $path = request()->file('company_logo')->store('logos', 'public');
+
+            $validatedData['company_logo'] = $path;
+        }
+
+        $job->update($validatedData);
+
+        return redirect()->route('jobs.show', $job->id)->with('success', 'Job updated successfully');
     }
 
     /**
--- resources/views/components/inputs/text-area.blade.php
+++ resources/views/components/inputs/text-area.blade.php
@@ -3,7 +3,7 @@
     'name',
     'label' => null,
     'type' => 'text',
-    'value',
+    'value' => '',
     'placeholder' => '',
     'large' => false
 ])
@@ -14,7 +14,7 @@
     @endif
     <textarea  @if($large) cols="30" rows="7" @endif  id="{{ $id }}" name="{{ $name }}"
         class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror"
-        placeholder="{{ $placeholder }}">{{ old($name) }}</textarea>
+        placeholder="{{ $placeholder }}">{{ old($name, $value) }}</textarea>
     @error($name)
         <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
     @enderror
