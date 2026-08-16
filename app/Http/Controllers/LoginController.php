b1df3db2568cf8015b50a6d23f67a6fee3b36f62
Alireza Esp <alireza.esp07@gmail.com>
2026-08-16 14:18:54 +0330

    add auth function in login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class LoginController extends Controller
 {
     public function login(Request $request): View {
         return view('auth.login');
+    }
+
+    public function auth(Request $request): RedirectResponse {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('home')->with('success', 'You are now logged in!');
+        }
+
+        return back()->withErrors([
+            'email' => 'Credentials are not valid.'
+        ])->withInput($request->only('email'));
     }
 }
