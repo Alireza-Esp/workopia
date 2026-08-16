6922b02cbe44cd9118aa97dfac864f46ff1d6ea4
Alireza Esp <alireza.esp07@gmail.com>
2026-08-16 12:58:29 +0330

    add register and login routes, controllers and views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,8 @@ use App\Models\Job;
 class HomeController extends Controller
 {
     public function index() {
-        $jobs = Job::latest()->limit(6)->get()
-;
+        $jobs = Job::latest()->limit(6)->get();
+
         return view('pages.index')->with('jobs', $jobs);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\JobController;;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::resource('jobs', JobController::class);
+
+Route::get('/register', [RegisterController::class, 'register']);
+Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'auth']);
