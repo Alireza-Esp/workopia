1106c6d2f5ae1b14567ad6788ace5ce7f7bbf288
Alireza Esp <alireza.esp07@gmail.com>
2026-08-17 20:55:29 +0330

    add avatar image in header component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,8 +8,17 @@
             <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
             @auth
             <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
-            <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
+            <!-- <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link> -->
             <x-logout-button></x-logout-button>
+            <div class="flex items-center space-x-3">
+                <a href="{{ route('dashboard') }}">
+                    @if (Auth::user()->avatar)
+                        <img src="{{ asset('storage/' . Auth::user()->avatar) }}" alt="{{ Auth::user()->name }}" class="w-10 h-10 rounded-full">
+                    @else
+                        <img src="{{ asset('storage/avatars/avatar.jpeg') }}" alt="{{ Auth::user()->name }}" class="w-10 h-10 rounded-full">
+                    @endif
+                </a>
+            </div>
             <x-button-link url='/jobs/create' icon='pen-to-square'>Create Job</x-button-link>
             @else
             <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
@@ -20,7 +29,6 @@
             <i class="fa fa-bars text-2xl"></i>
         </button>
     </div>
-    <!-- Mobile Menu -->
     <nav x-show="open" @click.away="open = false" id="mobile-menu" class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
         <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
         @auth
