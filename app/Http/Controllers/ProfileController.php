491664a4658051069e8ccb7f9ea6f212450be00b
Alireza Esp <alireza.esp07@gmail.com>
2026-08-17 17:06:58 +0330

    add avatar filed to database and model via new migration, and add profile upload and show functionality to dashboard page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileController extends Controller
 {
@@ -14,10 +14,23 @@ class ProfileController extends Controller
 
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email'
+            'email' => 'required|string|email',
+            'avatar' => 'nullable|image|mimes:png,jpg,jpeg|max:2048'
         ]);
 
-        $user->update($validatedData);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        if ($request->hasFile('avatar')) {
+            if ($user->avatar && Storage::disk('public')->exists($user->avatar)) {
+                Storage::disk('public')->delete($user->avatar);
+            }
+
+            $avatarPath = $request->file('avatar')->store('avatars', 'public');
+            $user->avatar = $avatarPath;
+        }
+
+        $user->save();
 
         return redirect()->route('dashboard')->with('success', 'Your profile updated successfully.');
     }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::resource('jobs', JobController::class)->middleware('auth')->only('create', 'store', 'edit', 'update', 'destroy');
