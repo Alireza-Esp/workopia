34937ea29f77d18e7e3c06f87383fe307342826d
Alireza Esp <alireza.esp07@gmail.com>
2026-08-18 12:35:30 +0330

    add bookmarks table migration and relations in Job and User models

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -33,5 +34,9 @@ class User extends Authenticatable
 
     public function jobListings(): HasMany {
         return $this->hasMany(Job::class);
+    }
+
+    public function bookmarkedJobs(): BelongsToMany {
+        return $this->belongsToMany(Job::class, 'job_user_bookmarks')->withTimestamps();
     }
 }
