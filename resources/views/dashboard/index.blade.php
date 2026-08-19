a293b587b872c4357362780372c1c1071bfb01b9
Alireza Esp <alireza.esp07@gmail.com>
2026-08-19 17:33:06 +0330

    add applicants list in dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -28,7 +28,7 @@
                 My Job Listings
             </h3>
             @forelse ($jobs as $job)
-                <div class="flex justify-between items-center border-b-2 border-gray-200 py-2">
+                <div class="flex justify-between items-center border-b-2 border-gray-200 py-2 mt-4">
                     <div>
                         <h3 class="text-xl font-semibold">
                             {{ $job->title }}
@@ -48,8 +48,35 @@
                         </form>
                     </div>
                 </div>
-            @empty
 
+                <div class="mt-1">
+                    <h4 class="text-lg font-semibold mb-2">
+                        Applicants
+                    </h4>
+                    @forelse ($job->applicants as $applicant)
+                        <p class="text-gray-800">
+                            <strong>Name: </strong>{{ $applicant->full_name }}
+                        </p>
+                        <p class="text-gray-800">
+                            <strong>Phone: </strong>{{ $applicant->contact_phone }}
+                        </p>
+                        <p class="text-gray-800">
+                            <strong>Email: </strong>{{ $applicant->contact_email }}
+                        </p>
+                        <p class="text-gray-800">
+                            <strong>Message: </strong>{{ $applicant->message }}
+                        </p>
+                        <p class="text-gray-800">
+                            <a href="{{ asset('storage/' . $applicant->resume_path) }}" class="text-blue-500 hover:underline" download>
+                                <i class="fa fa-download" mr-1></i> Download Resume
+                            </a>
+                        </p>
+                    @empty
+                        <p class="text-gray-700">No applicants for this job</p>
+                    @endforelse
+            </div>
+            @empty
+                <p class="text-gray-700">You have not job listing</p>
             @endforelse
         </div>
     </section>
