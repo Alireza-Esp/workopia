15cd2bd1264554f7c5340de538b458657dc2a01b
Alireza Esp <alireza.esp07@gmail.com>
2026-08-19 13:39:53 +0330

    add apply modal to job listing show page

--- resources/views/components/inputs/file.blade.php
+++ resources/views/components/inputs/file.blade.php
@@ -3,6 +3,7 @@
     'name',
     'value' => '',
     'label' => null,
+    'required' => false
 ])
 
 <div class="mb-4">
@@ -10,7 +11,7 @@
         <label class="block text-gray-700" for="{{ $id }}">{{ $label }}</label>
     @endif
     <input id="{{ $id }}" type="file" name="{{ $name }}"
-        class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror" value={{ old($name, $value) }}/>
+        class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror" value={{ old($name, $value) }} {{ $required ? 'required' : '' }}/>
     @error($name)
         <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
     @enderror
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -77,10 +77,34 @@
                     Put "Job Application" as the subject of your email
                     and attach your resume.
                 </p>
-                <a href="mailto:{{ $job->contact_email }}"
-                    class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
-                    Apply Now
-                </a>
+                <div x-data="{open: false}">
+                    <button @click="open = true"
+                        class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+                        Apply Now
+                    </button>
+                    <div x-show="open" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+                        <div @click.away="open = false" class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+                            <h3 class="text-lg font-semibold mb-4">
+                                Apply For {{ $job->title }}
+                            </h3>
+                            <form enctype="multipart/form-data">
+                                @csrf
+                                <x-inputs.text id="full_name" name="full_name" label="Full Name" :required="true"></x-inputs.text>
+                                <x-inputs.text id="contact_phone" name="contact_phone" label="Contact Phone"></x-inputs.text>
+                                <x-inputs.text id="contact_email" name="contact_email" label="Contact Email" :required="true"></x-inputs.text>
+                                <x-inputs.text-area id="message" name="message" label="Message"></x-inputs.text-area>
+                                <x-inputs.text id="location" name="location" label="Location"></x-inputs.text>
+                                <x-inputs.file id="resume" name="resume" label="Resume (.pdf)" :required="true"></x-inputs.file>
+                                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">
+                                    Submit Application
+                                </button>
+                                <button @click="open = false" type="submit" class="bg-gray-300 hover:bg-gray-400 text-black px-4 py-2 rounded-md">
+                                    Cancel
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="bg-white p-6 rounded-lg shadow-md mt-6">
